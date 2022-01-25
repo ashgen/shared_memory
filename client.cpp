fe4ca3852f8d6976a9ee3bb9beb0737e3933a615
Krishna <krishnarapolu.sai@gmail.com>
2022-01-25 19:51:10 +0530

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -13,24 +13,20 @@
 int main ()
 {
     using namespace boost::interprocess;
-    shared_memory_object segment(open_or_create, "volModelSharedMem",read_only);
+    shared_memory_object segment(open_only, "volModelSharedMem",read_only);
     mapped_region region(segment, read_only);
     volModelCurve* data = reinterpret_cast<volModelCurve*>(region.get_address());
 
-  while(1){
-        try
-        {
-            std::cout << data->atmiv <<"," << data->rho<<","<<data->eta<<std::endl;
-            for(auto x:data->arrayOfVolStrikes){
-              if(x.valid)
-              std::cout<<x.token << "," << x.iv <<"," << x.modelIV<<std::endl;
-            }
-        }catch(std::exception &e)
-        {
+    while(1) {
+        try {
+            std::cout << data->atmiv << "," << data->arrayOfVolStrikes[5].valid << ",";
+            std::cout << data->arrayOfVolStrikes[5].iv << std::endl;
+        }
+        catch(std::exception &e) {
             std::cout<<" error  " << e.what() <<std::endl;
             shared_memory_object::remove("volModelSharedMem");
         }
-       std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
     return 0;
